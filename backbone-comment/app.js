a2454dad6768f4b655f9a96d33b795319512cc2f
anaketa <g.sambrotta@gmail.com>
2013-12-12 16:58:36 +0100

    Delete app.js from root and move it in app/

--- backbone-comment/app.js
+++
@@ -1,8 +0,0 @@
-var listCommentCollection = new ListCommentCollection();
-listCommentCollection.fetch();
-
-var commentView = new CommentView({
-	collection: listCommentCollection,
-	el: $('#app')
-});
-commentView.render();
